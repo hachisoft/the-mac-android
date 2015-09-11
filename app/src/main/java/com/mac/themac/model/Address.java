52238ec2ff5b83faa733107ac9425dc80f0da32a
Samir Jaiswal <sjaiswal@hachisoft.com>
2015-09-11 09:15:35 -0700

    Fill address, emergencycontact, invitation, and vehicle models

--- app/src/main/java/com/mac/themac/model/Address.java
+++ app/src/main/java/com/mac/themac/model/Address.java
@@ -1,13 +1,51 @@
 package com.mac.themac.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.mac.themac.model.firebase.FBModelObject;
+import com.mac.themac.utility.FirebaseHelper;
+
+import java.util.HashMap;
 
 /**
  * Created by Samir on 9/11/2015.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Address extends FBModelObject {
+    public String addressLine1;
+    public String addressLine2;
+    public String addressLine3;
+    public String city;
+    public String state;
+    public String country;
+    public String zip;
+    public String company;
+    public String memberProfile;
+
+    @JsonIgnore
+    public MemberProfile linkedMemberProfile;
+
     public Address() {
+    }
+
+    @Override
+    @JsonIgnore
+    public void loadLinkedObjects() {
+
+        if(memberProfile != null && !memberProfile.isEmpty()) {
+            loadLinkedObject(MemberProfile.class, FirebaseHelper.FBRootContainerNames.memberProfiles, memberProfile);
+        }
+
+    }
+
+    @JsonIgnore
+    @Override
+    protected void setLinkedObject(Class<? extends FBModelObject> targetObjectType,
+                                   FBModelObject modelObject, int secondaryIdentifier) {
+
+        if(targetObjectType.equals(MemberProfile.class) &&
+                modelObject instanceof MemberProfile) {
+            linkedMemberProfile = (MemberProfile) modelObject;
+        }
     }
 }
--- app/src/main/java/com/mac/themac/model/EmergencyContact.java
+++ app/src/main/java/com/mac/themac/model/EmergencyContact.java
@@ -1,13 +1,45 @@
 package com.mac.themac.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.mac.themac.model.firebase.FBModelObject;
+import com.mac.themac.utility.FirebaseHelper;
 
 /**
  * Created by Samir on 9/11/2015.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class EmergencyContact extends FBModelObject {
+    public String firstName;
+    public String lastName;
+    public String phone;
+    public String email;
+    public String memberProfile;
+
+    @JsonIgnore
+    public MemberProfile linkedMemberProfile;
+
     public EmergencyContact() {
+    }
+
+    @Override
+    @JsonIgnore
+    public void loadLinkedObjects() {
+
+        if(memberProfile != null && !memberProfile.isEmpty()) {
+            loadLinkedObject(MemberProfile.class, FirebaseHelper.FBRootContainerNames.memberProfiles, memberProfile);
+        }
+
+    }
+
+    @JsonIgnore
+    @Override
+    protected void setLinkedObject(Class<? extends FBModelObject> targetObjectType,
+                                   FBModelObject modelObject, int secondaryIdentifier) {
+
+        if(targetObjectType.equals(MemberProfile.class) &&
+                modelObject instanceof MemberProfile) {
+            linkedMemberProfile = (MemberProfile) modelObject;
+        }
     }
 }
