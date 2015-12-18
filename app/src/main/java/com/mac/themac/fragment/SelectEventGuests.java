ac0a5e01c4c755d34ebe8e2d199a67ee7e7a7e82
Bryan Dormaier <bryan.dormaier@gmail.com>
2015-12-18 13:07:27 -0800

    updates for event registration

---
+++ app/src/main/java/com/mac/themac/fragment/SelectEventGuests.java
@@ -0,0 +1,18 @@
+package com.mac.themac.fragment;
+
+import com.mac.themac.R;
+
+/**
+ * Created by Bryan on 12/18/2015.
+ */
+public class SelectEventGuests extends FragmentWithTopActionBar {
+    @Override
+    protected int getFragmentLayoutId() {
+        return R.layout.fragment_select_guests;
+    }
+
+    @Override
+    protected int getTitleResourceId() {
+        return 0;
+    }
+}
